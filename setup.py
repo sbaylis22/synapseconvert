59e1935a794295780b27b9f40af4a03bc90b43e9
Sam Baylis <sam.baylis@gmail.com>
2025-06-23 13:36:44 -0500

    Add argparse help support to upconvert and downconvert

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="synapseconvert",
-    version="0.1.4",
+    version="0.1.5",
     author="Samuel Baylis",
     author_email="request@biblicalstory.org",
     description="Tools for converting Synapse RTP JSON to spreadsheet and back.",
